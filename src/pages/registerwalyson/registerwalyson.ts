c5196b373c415e8367a2a0aacab453d9f0f9daa7
walysonfelipe <walysonfelipe25@gmail.com>
2021-01-14 15:57:27 -0300

    :bug:  registrando varios users

--- src/pages/registerwalyson/registerwalyson.ts
+++ src/pages/registerwalyson/registerwalyson.ts
@@ -20,7 +20,8 @@ export class RegisterwalysonPage {
   ano:number;
   link:string;
   dado:any= [];
-  key:string = "dado";
+  key:string = "dados";
+  objnovo: any= [];
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
     private storage: Storage 
@@ -28,84 +29,78 @@ export class RegisterwalysonPage {
      
   }
 
-  register(){
-    if(this.link != "" && this.nome != "" && this.senha.length >= 8 && this.ano != 0 && (2021- this.ano) >= 10){
-      
-      let obj = [{
-          nome: this.nome,
-          senha: this.senha,
-          link: this.link,
-          ano: this.ano
-      },
-      {
-        nome: this.nome,
-        senha: this.senha,
-        link: this.link,
-        ano: this.ano
-    }];
-
-      this.dado.push(obj);
-
-     // console.log(this.dado);
-
-     // this.storage.set(this.key, this.dado);
-    //  this.navCtrl.push('HomewalysonPage', {
-      // nome:this.nome,
-      // senha:this.senha,
-     //  link:this.link,
-      // ano:this.ano
-     //}); 
-    }
-    else{
-      alert("Algo de errado aconteceu");
-    }
-
-  
-  
-    
-     
-  }
-
-  openpage(){
-    this.navCtrl.push('LoginwalysonPage');
-  }
-  
-  teste(){
-    
-  this.storage.length().then(result =>{
-    console.log(result);
-    if(result > 0){
-    let obj = [{
-        nome: this.nome,
-        senha: this.senha,
-        link: this.link,
-        ano: this.ano
-    }];
-
-      this.storage.get("dados").then((val:any) =>{
-       let v = val;
-        v.push(obj);
-       console.log(v);
-      });
-        
-    }
-    else{
-    alert("array não existe");
-    }
+  getCookie(keys:string){
+    return new Promise((resolve, reject) => {
+      resolve(
+        this.storage.get(keys).then((ret)=>{
+          return ret;
+        })
+      );
     });
 
-
-//    let obj = [{
-   //   nome: this.nome,
-     // senha: this.senha,
-      //link: this.link,
-      //ano: this.ano
-  //}];
-  //let dados = [];
-  //dados.push(obj);
-  //this.storage.set("teste", );
+  }
+  setCookie(key:string, dao:any){
+    return new Promise((resolve, reject) => {
+      resolve(
+        this.storage.set(key,dao).then((retorno)=>{
+        return retorno;
+        })
+      );
+    });
   }
 
+  async setCookies(key:string, dados:any){
+    const data = await new Promise((resolve, reject) => {
+      this.getCookie(key).then((retorno: any) => {
+       let v = retorno.concat(dados);
+        console.log(v);
+        this.setCookie(this.key, v); 
+        /*
+        this.objnovo = retorno;
+        this.objnovo.push(dados);
+        this.setCookie(this.key, this.objnovo); */
+        resolve(true);
+      });
+    });
+    return data;
+  }
+  register(){
+    if(this.link != "" && this.nome != "" && this.senha.length >= 8 && this.ano != 0 && (2021- this.ano) >= 10){
+      let obj:any[] =  [{
+        nome: this.nome,
+        senha: this.senha,
+        link: this.link,
+        ano: this.ano
+      }];
+      
+      this.storage.get(this.key).then((ret:any)=>{
+        if(ret == null){
+          this.storage.set(this.key,obj);
+          return;
+        }
+        if(ret.length > 0 && ret != ''){
+          let objnovo = ret.concat(obj);
+          this.storage.set(this.key,objnovo);
+          return;
+        }
+      })
+       
+        } }
+      
+
+  openpage(){
+   // this.navCtrl.push('LoginwalysonPage');
+  /*this.storage.remove("dados").then((ret)=>{
+     console.log("apagado");
+   })
+*/
+
+   this.getCookie(this.key).then((retorno)=>{
+   console.log(retorno);
+   });
+  
+
+  }
   /*
   abrirgaleria(){
     const options: CameraOptions = {
