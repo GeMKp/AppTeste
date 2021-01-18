2158f4bdc73795664da6af3735d88e7e461fb5f1
eduduf <eduardoxavier9871@gmail.com>
2021-01-18 16:00:09 -0300

    eduardo

---
+++ src/pages/edu-array/edu-array.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EduArrayPage } from './edu-array';
+
+@NgModule({
+  declarations: [
+    EduArrayPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(EduArrayPage),
+  ],
+})
+export class EduArrayPageModule {}
